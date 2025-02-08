a554e16c8e19b251e4b0fd1001dfc71cc040596a
Anup-Nayak <gpnayak1930@gmail.com>
2025-02-08 14:45:38 +0530

    starter code

---
+++ Q2/sampling_sgd.py
@@ -0,0 +1,79 @@
+# Imports - you can add any other permitted libraries
+import numpy as np
+
+# You may add any other functions to make your code more modular. However,
+# do not change the function signatures (name and arguments) of the given functions,
+# as these functions will be called by the autograder.
+
+def generate(N, theta, input_mean, input_sigma, noise_sigma):
+    """
+    Generate normally distributed input data and target values
+    Note that we have 2 input features
+    Parameters
+    ----------
+    N : int
+        The number of samples to generate.
+        
+    theta : numpy array of shape (3,)
+        The true parameters of the linear regression model.
+        
+    input_mean : numpy array of shape (2,)
+        The mean of the input data.
+        
+    input_sigma : numpy array of shape (2,)
+        The standard deviation of the input data.
+        
+    noise_sigma : float
+        The standard deviation of the Gaussian noise.
+        
+    Returns
+    -------
+    X : numpy array of shape (N, 2)
+        The input data.
+        
+    y : numpy array of shape (N,)
+        The target values.
+    """
+    pass
+
+class StochasticLinearRegressor:
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y, learning_rate=0.01):
+        """
+        Fit the linear regression model to the data using Gradient Descent.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        y : numpy array of shape (n_samples,)
+            The target values.
+
+        learning_rate : float
+            The learning rate to use in the update rule.
+            
+        Returns
+        -------
+        List of Parameters: numpy array of shape (n_iter, n_features+1,)
+            The list of parameters obtained after each iteration of Gradient Descent.
+        """
+        pass
+    
+    def predict(self, X):
+        """
+        Predict the target values for the input data.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        Returns
+        -------
+        y_pred : numpy array of shape (n_samples,)
+            The predicted target values.
+        """
+        pass
---
+++ Q4/gda.py
@@ -0,0 +1,53 @@
+# Imports - you can add any other permitted libraries
+import numpy as np
+
+# You may add any other functions to make your code more modular. However,
+# do not change the function signatures (name and arguments) of the given functions,
+# as these functions will be called by the autograder.
+
+
+class GaussianDiscriminantAnalysis:
+    # Assume Binary Classification
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y, assume_same_covariance=False):
+        """
+        Fit the Gaussian Discriminant Analysis model to the data.
+        Remember to normalize the input data X before fitting the model.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        y : numpy array of shape (n_samples,)
+            The target labels - 0 or 1.
+        
+        learning_rate : float
+            The learning rate to use in the update rule.
+        
+        Returns
+        -------
+        Parameters: 
+            If assume_same_covariance = True - 3-tuple of numpy arrays mu_0, mu_1, sigma 
+            If assume_same_covariance = False - 4-tuple of numpy arrays mu_0, mu_1, sigma_0, sigma_1
+            The parameters learned by the model.
+        """
+        pass
+    
+    def predict(self, X):
+        """
+        Predict the target values for the input data.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        Returns
+        -------
+        y_pred : numpy array of shape (n_samples,)
+            The predicted target label.
+        """
+        pass
